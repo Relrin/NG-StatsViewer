ba4e88499706b236e4eb112894c97e62be8eb7ba
Valery Savich <relrin78@gmail.com>
2014-06-25 21:52:57 +0200

    Update client_gui.py

--- client_gui.py
+++ client_gui.py
@@ -5,6 +5,22 @@ import ConfigParser
 import HtmlParser
 from PyQt4 import QtGui, QtCore, uic
 
+def addDotsInNumbers(data):
+    '''
+        string with dot separated numbers, which inserted after every third digit from right to left
+    '''
+    result=''
+    data=str(data).split(' ')
+    for phrase in data:
+        if phrase.isdigit() and len(phrase)>3:
+            buf = phrase[::-1]
+            result += format(int(buf), ',d')[::-1]+" "
+        else:
+            result += phrase+" "
+    if result[-1] == ',':
+        result[-1] = ""
+    return str(result[:len(result)-1])
+
 class RightClickMenu(QtGui.QMenu):
     def __init__(self, parent=None):
         QtGui.QMenu.__init__(self, "Menu", parent)
@@ -21,7 +37,7 @@ class RightClickMenu(QtGui.QMenu):
 
         # options window
         self.__login = self.__password = self.__ip = ""
-        self.__update_per = 1
+        self.__update_per = self.__koeff = 1
         if os.path.exists("NG-StatsViewer.cfg"):
             try:
                 config = ConfigParser.RawConfigParser()
@@ -30,9 +46,10 @@ class RightClickMenu(QtGui.QMenu):
                 self.__password = config.get('UserData', 'password')
                 self.__ip = config.get('UpdateData', 'ip')
                 self.__update_per = int(config.get('UpdateData', 'update_time'))
+                self.__koeff = int(config.get('UpdateData', 'koeff'))
             except ConfigParser.NoOptionError, ex:
                 self.__login = self.__password = self.__ip = ""
-                self.__update_per = 1
+                self.__update_per = self.__koeff = 1
         
         self.window = uic.loadUi("ui/options.ui")
         self.window.LoginEdit.setText(self.__login)
@@ -74,6 +91,12 @@ class RightClickMenu(QtGui.QMenu):
             config.set('UpdateData', 'update_time', self.window.UpdateSpinBox.value())
         else:
             config.set('UpdateData', 'update_time', self.__update_per)
+          
+        if self.window.KoeffSpinBox.value() != self.__koeff:
+            showDialog = True
+            config.set('UpdateData', 'koeff', self.window.KoeffSpinBox.value())
+        else:
+            config.set('UpdateData', 'koeff', self.__koeff)    
             
         with open('NG-StatsViewer.cfg', 'wb') as configfile:
             config.write(configfile)
@@ -87,21 +110,28 @@ class RightClickMenu(QtGui.QMenu):
         self.window.PasswordEdit.setText(self.__password)
         self.window.IpLineEdit.setText(self.__ip)
         self.window.UpdateSpinBox.setValue(self.__update_per)
+        self.window.KoeffSpinBox.setValue(self.__koeff)
         self.window.hide()
 
     def exit(self):
         QtCore.QCoreApplication.instance().quit()
 
 class WorkThread(QtCore.QThread):
-    def __init__(self, login, password, ip, interval = 5):
+    def __init__(self, login, password, ip, koeff, interval = 5):
         QtCore.QThread.__init__(self)
         self.__parser = HtmlParser.StatisticsParser(login, password, ip)
         self.__interval = interval
+        self.__koeff = koeff
+        self.__previos_data = [0, 0, 0, 0, '0 kbps', '0 kbps']
  
     def run(self):
         while(True):
-            th_data = str(self.__parser.getNewStats())
-            self.emit(QtCore.SIGNAL('add(QString)'), th_data)
+            real_speed = "0"
+            th_data, errors = self.__parser.getNewStats()
+            if self.__previos_data[1] != 0:
+                real_speed = "%s kbps" % (abs(th_data[1] - self.__previos_data[1])*20)
+            self.__previos_data = th_data
+            self.emit(QtCore.SIGNAL('add(QString)'), str(th_data + [real_speed]))
             time.sleep(self.__interval - 0.25)
 
 class SystemTrayIcon(QtGui.QSystemTrayIcon):
@@ -120,14 +150,13 @@ class SystemTrayIcon(QtGui.QSystemTrayIcon):
     def click_trap(self, value):
         # left click!
         if value == self.Trigger: 
-            QtCore.QTimer.singleShot(1, self.app_stats)
+            QtCore.QTimer.singleShot(50, self.app_stats)
     
     def get_data(self, data_str):
         data = eval(str(data_str))
-        new_string = "%-18s  %+8s %+10s\n" % (" ", "Upload", "Download")
-        new_string += "%-10s  %12s %+12s\n" % (data[0][0][0], data[0][0][2][0], data[0][0][3][0])
-        new_string += "%-10s  %10s %+12s\n" % (data[0][1][0], data[0][1][2][0], data[0][1][3][0])
-        new_string += "%-10s  %10s %+12s\n" % (data[0][2][0], data[0][2][2][0], data[0][2][3][0])
+        new_string = "Tx B/s: %15s    Rx B/s: %15s\n" % (addDotsInNumbers(data[2]), addDotsInNumbers(data[3]))
+        new_string += "Downstream: %s    Upstream: %s\n" % (addDotsInNumbers(data[4]), addDotsInNumbers(data[5]))
+        new_string += "Real speed: %s" % addDotsInNumbers(data[6])
         self.string = new_string
     
     def app_init(self):
@@ -147,7 +176,7 @@ class SystemTrayIcon(QtGui.QSystemTrayIcon):
             QtCore.QTimer.singleShot(1, self.welcome)
             # read config
             login = password = ip = ""
-            update_per = 1
+            update_per = koeff = 1
             try:
                 config = ConfigParser.RawConfigParser()
                 config.read('NG-StatsViewer.cfg')
@@ -155,11 +184,12 @@ class SystemTrayIcon(QtGui.QSystemTrayIcon):
                 password = config.get('UserData', 'password')
                 ip = config.get('UpdateData', 'ip')
                 update_per = int(config.get('UpdateData', 'update_time'))
+                koeff = int(config.get('UpdateData', 'koeff'))
             except ConfigParser.NoOptionError, ex:
                 login = password = ip = ""
-                update_per = 1
+                update_per = koeff = 1
             # create thread and catching signal from him
-            self.thread = WorkThread(login, password, ip, update_per)
+            self.thread = WorkThread(login, password, ip, koeff, update_per)
             self.connect(self.thread, QtCore.SIGNAL("add(QString)"), self.get_data)
             self.thread.start()
 
