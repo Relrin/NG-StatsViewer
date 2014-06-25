63020e933626d06847e67e28146d9daadd2ab484
Valery Savich <relrin78@gmail.com>
2014-06-25 21:52:39 +0200

    Update HtmlParser.py

--- HtmlParser.py
+++ HtmlParser.py
@@ -10,7 +10,7 @@ class StatisticsParser():
         self.__ip = ip
         
         self.__top_level_url = "http://%s" % (self.__ip)
-        self.__trafficMeter =  self.__top_level_url + "/traffic_meter.htm"
+        self.__stattbl =  self.__top_level_url + "/RST_stattbl.htm"
         
         self.__InitAuth()
     
@@ -34,35 +34,24 @@ class StatisticsParser():
         """
             Get new statistics from modem
         """
-        parsed_rows = []
+        parsed_colums = []
         errors = []
         try:
-            html = urllib2.urlopen(self.__trafficMeter)
+            html = urllib2.urlopen(self.__stattbl)
             soup = BeautifulSoup(html.read())
             # get table with statistics and divide on rows (without table header)
-            rows = soup.find("table", border="1", cellspacing="0", cellpadding="0", width="100%").findAll("tr")[2:]
-            for row in rows:
-                # get columns on every row
-                cells = row.findAll("td")
-                # there we are getting data from every column
-                field_name = cells[0].find("span").getText().encode("utf-8")
-                Connection_Time = cells[1].find("span").getText().encode("utf-8")
-                Upload_Avg = cells[2].findAll("p")
-                Download_Avg = cells[3].findAll("p")
-                Total_Avg = cells[4].findAll("p")
-                
-                # prepare for appending to result list
-                upload_avg_list = []
-                download_avg_list = []
-                total_avg_list = []
-                for item in Upload_Avg:
-                    upload_avg_list.append(int(item.getText().replace('/', '').strip()))
-                for item in Download_Avg:
-                    download_avg_list.append(int(item.getText().replace('/', '').strip()))
-                for item in Total_Avg:
-                    total_avg_list.append(int(item.getText().replace('/', '').strip()))
-                    
-                parsed_rows.append((field_name, Connection_Time, upload_avg_list, download_avg_list, total_avg_list))
+            rows = soup.find("table", border="0", width="102%").findAll("tr")
+            # getting data from columns
+            # first step: getting TxPkts, RxPkts, Tx B/s, Rx B/s,
+            cells_packets = rows[4].findAll("td")
+            parsed_colums.append(int(cells_packets[2].find("span").getText().encode("utf-8")))
+            parsed_colums.append(int(cells_packets[3].find("span").getText().encode("utf-8")))
+            parsed_colums.append(int(cells_packets[5].find("span").getText().encode("utf-8")))
+            parsed_colums.append(int(cells_packets[6].find("span").getText().encode("utf-8")))
+            # second step: getting info about connection speed
+            cells_connection_speed = rows[13].findAll("td")
+            parsed_colums.append(cells_connection_speed[1].getText().encode("utf-8"))
+            parsed_colums.append(cells_connection_speed[2].getText().encode("utf-8"))
         except urllib2.HTTPError, ex:
             errors.append("HTTP Error %d: %s" % (ex.code, ex.msg))
-        return parsed_rows, errors
+        return parsed_colums, errors
